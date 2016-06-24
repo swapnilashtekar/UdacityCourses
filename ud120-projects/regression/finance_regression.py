5107b6c7181a318b099bf6803873aa70847a4474
Swapnil Ashtekar <swapashtekar@gmail.com>
2016-06-23 20:39:41 -0600

    Added regression quiz

--- ud120-projects/regression/finance_regression.py
+++ ud120-projects/regression/finance_regression.py
@@ -17,19 +17,23 @@ import sys
 import pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn.linear_model import LinearRegression
+import numpy as np
 dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
+
+#features_list = ["bonus", "long_term_incentive"]
 features_list = ["bonus", "salary"]
-data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+data = featureFormat( dictionary, features_list, remove_any_zeroes=True, sort_keys = '../tools/python2_lesson06_keys.pkl')
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,12 +42,30 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+reg = LinearRegression()
+reg.fit(feature_train, target_train)    
+
+reg.predict(feature_test)
 
 
 
+print "Slope is :" ,reg.coef_
+print "Intercept is :",reg.intercept_
 
+#Chekcing score on training data
+# The mean square error
+print("Train : Residual sum of squares: %.2f"
+    % np.mean((reg.predict(feature_train) - target_train) ** 2))
 
+# Explained variance score: 1 is perfect prediction
+print('Train : Variance score: %.2f' % reg.score(feature_train, target_train))
 
+# The mean square error
+print("Test : Residual sum of squares: %.2f"
+    % np.mean((reg.predict(feature_test) - target_test) ** 2))
+
+# Explained variance score: 1 is perfect prediction
+print('Test : Variance score: %.2f' % reg.score(feature_test, target_test))
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
@@ -65,6 +87,9 @@ try:
 except NameError:
     pass
 plt.xlabel(features_list[1])
+reg.fit(feature_test, target_test)
+print "Slope is :" ,reg.coef_
+plt.plot(feature_train, reg.predict(feature_train), color="b") 
 plt.ylabel(features_list[0])
 plt.legend()
 plt.show()
