a8a94a3963bf8da91d8043500cc41bc19473b496
Swapnil Ashtekar <swapashtekar@gmail.com>
2016-07-31 23:02:23 -0600

    Validation added

--- ud120-projects/evaluation/evaluate_poi_identifier.py
+++ ud120-projects/evaluation/evaluate_poi_identifier.py
@@ -21,11 +21,38 @@ data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") 
 ### add more features to features_list!
 features_list = ["poi", "salary"]
 
-data = featureFormat(data_dict, features_list)
+data = featureFormat(data_dict, features_list, sort_keys = '../tools/python2_lesson14_keys.pkl')
 labels, features = targetFeatureSplit(data)
 
 
 
 ### your code goes here 
 
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.tree import DecisionTreeClassifier
 
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.30, random_state=42)
+print "People in test size: ", len(features_test) 
+
+clf = DecisionTreeClassifier()
+
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+'''
+# Changing predicted ones to zero by redeclaring pred to all zeros
+import numpy as np
+pred = np.array ([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+'''
+acc = accuracy_score(labels_test, pred)
+
+print "Test Labels are: ", labels_test
+print "Prediction labels are: ", pred
+
+print "Accuracy: ", acc
+print "Number of poi's test: ", sum(pred)
+print "Precision: ", precision_score(labels_test, pred)
+print "Recall: ", recall_score(labels_test, pred)
